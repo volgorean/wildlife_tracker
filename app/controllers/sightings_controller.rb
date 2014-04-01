a14a9967cdf1d36de84987b6919d4ed08314b0b9
Peter Tinti <student@epicodus.com>
2014-04-01 16:09:00 -0700

    fixed listing error for both species and region

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -8,14 +8,6 @@ class SightingsController < ApplicationController
     @regions = Region.all
     @sightings = Sighting.all
     @sighting = Sighting.new
-    @speciesNames = [0]
-    Species.all.each do |single|
-      @speciesNames << single.name
-    end
-    @regionNames = [0]
-    Region.all.each do |single|
-      @regionNames << single.name
-    end
     render :new
   end
 
